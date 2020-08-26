a2f7769e70cf31d5ea35e5b900063dc189cc7677
bibhas <bibhas2@gmail.com>
2020-08-26 10:31:27 -0400

    Unit test reactive form

--- src/app/pet-display/pet-display.component.spec.ts
+++ src/app/pet-display/pet-display.component.spec.ts
@@ -1,6 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PetDisplayComponent } from './pet-display.component';
+import { Pet } from '../model/pet';
+import { By } from '@angular/platform-browser';
 
 describe('PetDisplayComponent', () => {
   let component: PetDisplayComponent;
@@ -16,10 +18,69 @@ describe('PetDisplayComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(PetDisplayComponent);
     component = fixture.componentInstance;
+
+    //Must supply @Input values here
+    let pet = new Pet
+
+    pet.id = "100"
+    pet.animalType = "Dog"
+    pet.name = "Scooby"
+
+    component.pet = pet
+
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('Should initialize form', () => {
+    expect(component.adoptionForm.controls.messageText.value)
+      .toBe(`I will like more information about Scooby`)
+  });
+
+  it('Form should be valid', () => {
+    component.adoptionForm.patchValue({
+      clientName: "Bibhas",
+      clientEmail: "bibhas2@gmail.com"
+    })
+
+    expect(component.adoptionForm.valid).toBeTrue()
+  });
+
+  it('Form should be invalid', () => {
+    component.adoptionForm.patchValue({
+      clientName: "Bibhas",
+      clientEmail: "bibhas2@ibm.com"
+    })
+
+    expect(component.adoptionForm.valid).toBeFalse()
+  });
+
+  it("Should submit form", () => {
+    component.adoptionForm.patchValue({
+      clientName: "Bibhas",
+      clientEmail: "bibhas2@gmail.com"
+    })
+
+    //Open the popup form
+    component.showPopup = true
+    //Need to call this after altering component's state
+    fixture.detectChanges();
+
+    //Lookup the form
+    let form = fixture.debugElement.query(By.css("form"))
+    //Trigger submit event
+    form.triggerEventHandler("submit", {})
+    
+    fixture.detectChanges();
+
+    //Form should be reset now.
+    expect(component.adoptionForm.controls.clientName.value)
+      .toBe("")
+
+    //Popup should be closed now
+    expect(component.showPopup).toBeFalse()
+  })
 });
